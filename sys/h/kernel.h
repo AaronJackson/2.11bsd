2b30a0c113a98b6696302d4ec611b402c0e59af4
brent hartshorn <brenthartshorn@proton.me>
2024-09-01 14:52:11 -0700

    RISC-V extern fixes

--- sys/h/kernel.h
+++ sys/h/kernel.h
@@ -9,28 +9,50 @@
 /*
  * Global variables for the kernel
  */
+#ifdef __riscv
+	#ifdef SUPERVISOR
+		extern long startnet;			/* start of network data space */
+	#else
+		memaddr	malloc();
+		/* 1.1 */
+		long	hostid;
+		char	hostname[MAXHOSTNAMELEN];
+		int	hostnamelen;
+		/* 1.2 */
+		#include <sys/time.h>
+		struct	timeval boottime;
+		struct	timeval time;
+		struct	timezone tz;			/* XXX */
+		int	adjdelta;
+		int	hz;
+		int	mshz;				/* # milliseconds per hz */
+		int	lbolt;				/* awoken once a second */
+		int	realitexpire();
+		short	avenrun[3];
+	#endif
 
-#ifdef SUPERVISOR
-long	startnet;			/* start of network data space */
 #else
-memaddr	malloc();
+	#ifdef SUPERVISOR
+		long	startnet;			/* start of network data space */
+	#else
+		memaddr	malloc();
 
-/* 1.1 */
-long	hostid;
-char	hostname[MAXHOSTNAMELEN];
-int	hostnamelen;
+		/* 1.1 */
+		long	hostid;
+		char	hostname[MAXHOSTNAMELEN];
+		int	hostnamelen;
 
-/* 1.2 */
-#include <sys/time.h>
+		/* 1.2 */
+		#include <sys/time.h>
 
-struct	timeval boottime;
-struct	timeval time;
-struct	timezone tz;			/* XXX */
-int	adjdelta;
-int	hz;
-int	mshz;				/* # milliseconds per hz */
-int	lbolt;				/* awoken once a second */
-int	realitexpire();
-
-short	avenrun[3];
+		struct	timeval boottime;
+		struct	timeval time;
+		struct	timezone tz;			/* XXX */
+		int	adjdelta;
+		int	hz;
+		int	mshz;				/* # milliseconds per hz */
+		int	lbolt;				/* awoken once a second */
+		int	realitexpire();
+		short	avenrun[3];
+	#endif
 #endif
